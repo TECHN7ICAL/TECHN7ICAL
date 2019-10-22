e0f40d2ab4e61ee2065908302fe2e5c738eae7ed
TECHN7ICAL <38501938+TECHN7ICAL@users.noreply.github.com>
2019-10-22 19:38:21 +0300

    Create r21.js

---
+++ r21.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($)
+{
+	$( '.one-page-layout .site-main > section:nth-of-type(1)' ).toggleClass( 'light-text' );
+});
+</script>
+			
+<!-- 3. Page Background Color -->
+<style type="text/css">
+.one-page-layout .site-main > section:nth-of-type(3) { background-color: #ebf0df; }
